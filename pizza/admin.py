2cbe33849ba7c5b147e288b69b97994bf11fcce8
demchyshynv <K1l@t1V1>
2020-12-23 02:21:51 +0200

    lesson1-hw

--- pizza/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from .models import PizzaShopModel, PizzaModel
-
-# Register your models here.
-admin.site.register(PizzaShopModel)
-admin.site.register(PizzaModel)
--- pizza/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-
-from .views import pizza_shop
-urlpatterns = [
-    path('', pizza_shop)
-]
